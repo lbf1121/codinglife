93f36e498ec4440683cb9f6ab0710bafb794697f
lbf1121 <lbf1121@gmail.com>
2018-06-22 14:17:02 +0800

    因数、倍数、完全数

--- mathematics/JuniorMathematics.md
+++ mathematics/JuniorMathematics.md
@@ -1,2 +1,25 @@
 初中数学
 ============
+1.因数、倍数、完全数
+------------
+  概念：2 * 6 = 12，2、6 是 12 的因数，12 是 2、6 的倍数。 完全数：6 = 1 + 2 + 3
+
+  [java] 查找1000以下的完全数。
+  解析：迭代每一个数，并计算出所有小于当前数并能被这个数模运算等于0的数的和，如果和等于这个数，那么这个数就是完全数。
+  static List findPerfectNumber(int num){
+        List rs = new ArrayList();
+        for(int i=2;i<=num;i++){
+            int a = i;
+            int sum = 0;
+            for(int j=1;j<i;j++){
+                if(a%j==0){
+                    sum += j;
+                }
+            }
+
+            if(a==sum){
+                rs.add(a);
+            }
+        }
+        return rs;
+   }
---
+++ mathematics/PerfectNumber.java
@@ -0,0 +1,27 @@
+package mathematics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PerfectNumber {
+   static List findPerfectNumber(int num){
+        List rs = new ArrayList();
+        for(int i=2;i<=num;i++){
+            int a = i;
+            int sum = 0;
+            for(int j=1;j<i;j++){
+                if(a%j==0){
+                    sum += j;
+                }
+            }
+
+            if(a==sum){
+                rs.add(a);
+            }
+        }
+        return rs;
+   }
+   public static void main(String[] args){
+        System.out.print(findPerfectNumber(10000));
+   }
+}
